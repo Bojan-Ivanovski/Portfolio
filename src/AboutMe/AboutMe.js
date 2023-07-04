5a5a6a39f72390236491a49ba205eb37c92236e8
Bojan-Ivanovski <0ivanovskibojan@gmail.com>
2023-07-05 00:11:02 +0200

    Fix Host

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -12,7 +12,7 @@ export default function About({onThis})
             <div>
                 <h1>Hello, I'm Bojan Ivanovski</h1>
                 <p>I'm a versatile developer with a passion for technology and a deep understanding of computers. As a Computer Engineering major, I have acquired a diverse skill set that allows me to excel in various aspects of software development and IT.</p>
-                <a href="/#/MoreAboutMe"><button>Show me more</button></a>
+                <a href="Portfolio/#/MoreAboutMe"><button>Show me more</button></a>
             </div>
         </div>
         </>
